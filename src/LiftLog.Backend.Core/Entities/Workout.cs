002053b9ecf19002b988d2321dff012f3cf7dd44
Pedro Previatti <pvlpreviatti@gmail.com>
2025-09-23 23:21:13 -0300

    chore: added new workout class and validator

---
+++ src/LiftLog.Backend.Core/Entities/Workout.cs
@@ -0,0 +1,75 @@
+using System.Diagnostics.CodeAnalysis;
+using LiftLog.Backend.Core.Validators;
+
+namespace LiftLog.Backend.Core.Entities;
+
+public class Workout : BaseEntity
+{
+    public required List<Guid> WorkoutExerciseIds { get; set; }
+    public required List<Guid> CreatedForUserIds { get; set; }
+
+    public required string Name { get; set; }
+
+    protected Workout() { }
+
+    [SetsRequiredMembers]
+    private Workout(
+        List<Guid> workoutExerciseIds,
+        List<Guid> createdForUserIds,
+        Guid createdBy,
+        string name
+    )
+    {
+        CreatedBy = createdBy;
+        UpdatedBy = createdBy;
+
+        CreatedForUserIds = createdForUserIds;
+        WorkoutExerciseIds = workoutExerciseIds;
+        Name = name;
+
+        Validate(this, new WorkoutValidator());
+    }
+
+    public static Workout Create(
+        List<Guid> workoutExerciseIds,
+        List<Guid> createdForUserIds,
+        Guid createdBy,
+        string name
+    ) => new(workoutExerciseIds, createdForUserIds, createdBy, name);
+
+    public void Update(
+        Guid updatedBy,
+        List<Guid>? workoutExerciseIds = null,
+        List<Guid>? createdForUserIds = null,
+        string? name = null
+    )
+    {
+        UpdatedBy = updatedBy;
+        UpdatedAtUtc = DateTime.UtcNow;
+
+        if (workoutExerciseIds is not null && workoutExerciseIds.Count > 0)
+            WorkoutExerciseIds.AddRange(
+                workoutExerciseIds.Where(exerciseId => !WorkoutExerciseIds.Contains(exerciseId))
+            );
+
+        if (createdForUserIds is not null && createdForUserIds.Count > 0)
+            CreatedForUserIds.AddRange(
+                createdForUserIds.Where(userId => !WorkoutExerciseIds.Contains(userId))
+            );
+
+        if (!string.IsNullOrWhiteSpace(name))
+            Name = name;
+
+        Validate(this, new WorkoutValidator());
+    }
+
+    public void RemoveUserFromWorkout(Guid updatedBy, Guid userId)
+    {
+        UpdatedBy = updatedBy;
+        UpdatedAtUtc = DateTime.UtcNow;
+
+        WorkoutExerciseIds.Remove(userId);
+
+        Validate(this, new WorkoutValidator());
+    }
+}
---
+++ src/LiftLog.Backend.Core/Validators/WorkoutValidator.cs
@@ -0,0 +1,36 @@
+using FluentValidation;
+using LiftLog.Backend.Core.Entities;
+
+namespace LiftLog.Backend.Core.Validators;
+
+public class WorkoutValidator : AbstractValidator<Workout>
+{
+    public WorkoutValidator()
+    {
+        RuleFor(x => x.CreatedBy)
+            .NotNull()
+            .WithMessage("CreatedBy is required.")
+            .Must(id => id != Guid.Empty)
+            .WithMessage("CreatedBy must be a valid Guid.");
+
+        RuleFor(x => x.WorkoutExerciseIds)
+            .NotNull()
+            .WithMessage("WorkoutExerciseIds is required.")
+            .Must(list => list is not null && list.Count > 0 && list.All(g => g != Guid.Empty))
+            .WithMessage("WorkoutExerciseIds must be a valid list of Guids.");
+
+        RuleFor(x => x.CreatedForUserIds)
+            .NotNull()
+            .WithMessage("CreatedForUserIds is required.")
+            .Must(list => list is not null && list.Count > 0 && list.All(g => g != Guid.Empty))
+            .WithMessage("CreatedForUserIds must be a valid list of Guids.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.")
+            .MinimumLength(3)
+            .WithMessage("Name must have more than 2 characters.")
+            .MaximumLength(30)
+            .WithMessage("Name must be up to 30 characters.");
+    }
+}
