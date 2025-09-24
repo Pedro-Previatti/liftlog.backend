6d1d7136ce676c7ea26b26add4e0d1f600bf395c
Pedro Previatti <pvlpreviatti@gmail.com>
2025-09-23 23:12:00 -0300

    chore: added new base entity class

---
+++ src/LiftLog.Backend.Core/Entities/BaseEntity.cs
@@ -0,0 +1,35 @@
+using FluentValidation;
+using FluentValidation.Results;
+using LiftLog.Backend.Core.Helpers;
+
+namespace LiftLog.Backend.Core.Entities;
+
+public class BaseEntity
+{
+    public required Guid Id { get; init; }
+    public Guid CreatedBy { get; init; }
+    public Guid UpdatedBy { get; set; }
+
+    public required DateTime CreatedAtUtc { get; init; } = DateTime.UtcNow;
+    public required DateTime UpdatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    private readonly NotificationContext _notificationContext = new();
+
+    public ValidationResult? ValidationResult { get; private set; }
+
+    public bool Valid { get; private set; }
+    public bool Invalid => !Valid;
+
+    protected bool Validate<TModel>(TModel model, AbstractValidator<TModel> validator)
+    {
+        ValidationResult = validator.Validate(model);
+        Valid = ValidationResult.IsValid;
+
+        if (Invalid)
+            _notificationContext.AddNotifications(ValidationResult);
+
+        return Valid;
+    }
+
+    public IReadOnlyList<Notification> GetNotifications() => _notificationContext.Notifications;
+}
