aacd4e268aac216d2a7923c476f8bcc8b43fb98e
Pedro Previatti <pvlpreviatti@gmail.com>
2025-10-10 23:31:47 -0300

    chore: added constant names for muscle group and exercise names

---
+++ src/LiftLog.Backend.Core/Shared/MuscleGroupsNameConstants.cs
@@ -0,0 +1,20 @@
+namespace LiftLog.Backend.Core.Shared;
+
+public static class MuscleGroupsNameConstants
+{
+    public const string Adductors = "Adductors";
+    public const string Abductors = "Abductors";
+    public const string Back = "Back";
+    public const string Biceps = "Biceps";
+    public const string Calves = "Calves";
+    public const string Chest = "Chest";
+    public const string Core = "Core";
+    public const string Forearms = "Forearms";
+    public const string Glutes = "Glutes";
+    public const string Hamstrings = "Hamstrings";
+    public const string Neck = "Neck";
+    public const string Quads = "Quads";
+    public const string Shoulders = "Shoulders";
+    public const string Traps = "Traps";
+    public const string Triceps = "Triceps";
+}
