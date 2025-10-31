8c1d655418702d43cab384d6847b544c0a6e1e70
Pedro Previatti <pvlpreviatti@gmail.com>
2025-10-31 17:00:02 -0300

    chore: added new enum to filter muscle groups

---
+++ src/LiftLog.Backend.Core/Enums/MuscleGroupParam.cs
@@ -0,0 +1,20 @@
+namespace LiftLog.Backend.Core.Enums;
+
+public enum MuscleGroupParam
+{
+    Abductors = 10,
+    Adductors = 20,
+    Back = 30,
+    Biceps = 40,
+    Calves = 50,
+    Chest = 60,
+    Core = 70,
+    Forearms = 80,
+    Glutes = 90,
+    Hamstrings = 100,
+    Neck = 110,
+    Quads = 120,
+    Shoulders = 130,
+    Traps = 140,
+    Triceps = 150,
+}
