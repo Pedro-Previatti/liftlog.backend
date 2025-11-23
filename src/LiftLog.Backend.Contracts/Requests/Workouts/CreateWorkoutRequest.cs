65a9310e57a25780be67fb1471e3955e0818c402
Pedro Previatti <pvlpreviatti@gmail.com>
2025-11-22 23:41:31 -0300

    chore: created workout creation request

---
+++ src/LiftLog.Backend.Contracts/Requests/Workouts/CreateWorkoutRequest.cs
@@ -0,0 +1,11 @@
+using LiftLog.Backend.Contracts.Responses;
+using LiftLog.Backend.Contracts.Responses.Workouts;
+using LiftLog.Backend.Core.Enums;
+using MediatR;
+
+namespace LiftLog.Backend.Contracts.Requests.Workouts;
+
+public record CreateWorkoutRequest(string Name, List<WorkoutExerciseRequest> Exercises)
+    : IRequest<Response<WorkoutResponse>>;
+
+public record WorkoutExerciseRequest(Guid Id, int Sets, int Reps, float Weight, WeightUnit Unit);
